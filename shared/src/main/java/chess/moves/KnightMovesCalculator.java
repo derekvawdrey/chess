a1bcb3fe5312c1dbe8232e74ef851f09f5e64e35
derekvawdrey <derek.vawdrey@gmail.com>
2024-09-06 15:23:23 -0600

    Fixing rest of Knight

--- shared/src/main/java/chess/moves/KnightMovesCalculator.java
+++ shared/src/main/java/chess/moves/KnightMovesCalculator.java
@@ -21,7 +21,7 @@ public class KnightMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
         List<ChessMove> moves = new ArrayList<>();
         List<ChessPosition> positions = new ArrayList<>();
-        
+
         positions.add(new ChessPosition(position.getRow()+1, position.getColumn()-2));
         positions.add(new ChessPosition(position.getRow()+2, position.getColumn()-1));
         positions.add(new ChessPosition(position.getRow()+1, position.getColumn()+2));
@@ -32,14 +32,17 @@ public class KnightMovesCalculator implements PieceMovesCalculator {
         positions.add(new ChessPosition(position.getRow()-2, position.getColumn()+1));
 
         positions.forEach(new_position -> {
-            if(board.hasPiece(new_position) && !board.isPieceSameColor(board.getPiece(new_position),board.getPiece(position))){
-                moves.add(new ChessMove(position,new_position,null));
-            } else if(!board.hasPiece(new_position)){
-                moves.add(new ChessMove(position,new_position,null));
+
+            if((new_position.getColumn() > 0 && new_position.getColumn() < 9) && (new_position.getRow() > 0 && new_position.getRow() < 9)) {
+                if(board.hasPiece(new_position) && !board.isPieceSameColor(board.getPiece(new_position),board.getPiece(position))){
+                    moves.add(new ChessMove(position,new_position,null));
+                } else if(!board.hasPiece(new_position)){
+                    moves.add(new ChessMove(position,new_position,null));
+                }
             }
         });
 
 
-        return List.of();
+        return moves;
     }
 }

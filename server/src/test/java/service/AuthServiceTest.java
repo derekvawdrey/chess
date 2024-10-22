500a5b18a2b8798eb5cfcb172d3c1b979363741a
Derek Vawdrey <derekvawdrey@dereknoair.lan>
2024-10-21 18:40:12 -0600

    Adding test structure

--- server/src/test/java/service/AuthServiceTest.java
+++ server/src/test/java/service/AuthServiceTest.java
@@ -36,35 +36,59 @@ public class AuthServiceTest {
     @Test
     @Order(1)
     @DisplayName("Login Success")
+    public void loginSuccess() throws DataAccessException {
+
+    }
 
 
     @Test
     @Order(2)
     @DisplayName("Login Failure")
+    public void loginFailure() throws DataAccessException {
+
+    }
 
 
     @Test
     @Order(3)
     @DisplayName("Get Auth Data Success")
+    public void getAuthDataSuccess() throws DataAccessException {
+
+    }
 
     @Test
     @Order(4)
     @DisplayName("Get Auth Data Failure")
+    public void getAuthDataFailure() throws DataAccessException {
+
+    }
 
     @Test
     @Order(5)
-    @DisplayName("Normal User Login")
+    @DisplayName("Logout success")
+    public void logoutSuccess() throws DataAccessException {
+
+    }
 
     @Test
     @Order(6)
-    @DisplayName("Normal User Login")
+    @DisplayName("Logout Fail")
+    public void logoutFail() throws DataAccessException {
+
+    }
 
     @Test
     @Order(7)
-    @DisplayName("Normal User Login")
+    @DisplayName("deleteAll Success")
+    public void deleteAllSuccess() throws DataAccessException {
+
+    }
 
     @Test
     @Order(8)
-    @DisplayName("Normal User Login")
+    @DisplayName("deleteAll Fail")
+    public void deleteAllFail() throws DataAccessException {
+        
+    }
 
 }

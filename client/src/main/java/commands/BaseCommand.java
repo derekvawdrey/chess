401ffc4aff73b7aff065d91edb6ca1127d436eca
Derek Vawdrey <derekvawdrey@dereknoair.lan>
2024-11-11 07:31:26 -0700

    Starting ChessClient

--- client/src/main/java/commands/BaseCommand.java
+++ client/src/main/java/commands/BaseCommand.java
@@ -11,7 +11,7 @@ public abstract class BaseCommand {
         this.commandString = commandString;
         this.numberOfArguments = numberOfArguments;
     }
-    
+
     protected void executeCommand(String[] args){
 
     }
--- client/src/main/java/model/ChessClient.java
+++ client/src/main/java/model/ChessClient.java
@@ -1,4 +1,24 @@
 package model;
 
-public record ChessClient() {
+import commands.BaseCommand;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+public class ChessClient {
+    protected final HashMap<String, BaseCommand> preLoginCommands;
+    protected final HashMap<String, BaseCommand> postLoginCommands;
+
+    public ChessClient() {
+        this.preLoginCommands = new HashMap<>();
+        this.postLoginCommands = new HashMap<>();
+    }
+
+    public static enum LoginState{
+        PRE_LOGIN,
+        POST_LOGIN
+    }
+
+
 }

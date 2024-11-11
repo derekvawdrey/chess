b29c1ee012074021132a08ddde6241e799c62d5a
Derek Vawdrey <derekvawdrey@dereknoair.lan>
2024-11-11 13:43:40 -0700

    Finishing tests up

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -5,9 +5,7 @@ import dataaccess.DataAccessException;
 import dataaccess.DataAccessType;
 import dataaccess.manager.DataAccessManager;
 import exception.ResponseException;
-import model.AuthData;
-import model.LoginRequest;
-import model.UserData;
+import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
 import service.UserService;
@@ -82,49 +80,114 @@ public class ServerFacadeTests {
     @Test
     @DisplayName("Logout - positive")
     public void logoutPositiveTest() throws ResponseException {
-
+        UserData userData = new UserData("a", "a", "a");
+        AuthData authData = serverFacade.register(userData);
+        serverFacade.logout(authData.authToken());
+        try{
+            serverFacade.listGames(authData.authToken());
+            Assertions.fail("Auth token should be deleted.");
+        } catch (ResponseException e) {
+            Assertions.assertEquals(401, e.getStatusCode(), "Status code 401 expected, because the auth no longer exists");
+        }
     }
 
     @Test
     @DisplayName("Logout - negative")
     public void logoutNegativeTest() throws ResponseException {
-
+        try{
+            serverFacade.logout("fake auth token");
+            Assertions.fail("Auth token should be deleted.");
+        } catch (ResponseException e) {
+            Assertions.assertEquals(401, e.getStatusCode(), "Status code 401 expected, because the auth never existed.");
+        }
     }
 
     @Test
     @DisplayName("List Games - positive")
     public void listGamesPositiveTest() throws ResponseException {
+        UserData userData = new UserData("a", "a", "a");
+        AuthData authData = serverFacade.register(userData);
 
+        GameListResult games = serverFacade.listGames(authData.authToken());
+        Assertions.assertEquals(0, games.getGames().size());
     }
 
     @Test
     @DisplayName("List Games - negative")
     public void listGamesNegativeTest() throws ResponseException {
-
+        try{
+            serverFacade.listGames("");
+            Assertions.fail("Shouldn't be able to access without valid auth token.");
+        } catch (ResponseException e) {
+            Assertions.assertEquals(401, e.getStatusCode(), "Status code 401 expected, because the auth never existed.");
+        }
     }
 
     @Test
-    @DisplayName("Join Game - positive")
+    @DisplayName("Join Game - positive and negative")
     public void joinGamePositiveTest() throws ResponseException {
+        UserData userData = new UserData("a", "a", "a");
+        AuthData authData = serverFacade.register(userData);
+
+        serverFacade.createGame(new CreateGameRequest("newgame"), authData.authToken());
+
+        Assertions.assertDoesNotThrow(() -> serverFacade.joinGame(new JoinGameRequest(
+                "WHITE",
+                "a",
+                1
+        ), authData.authToken()));
+
+        Assertions.assertThrows(ResponseException.class, () -> serverFacade.joinGame(new JoinGameRequest(
+                "WHITE",
+                "a",
+                1
+        ), authData.authToken()));
+    }
+
+    @Test
+    @DisplayName("Join Game - negative 2")
+    public void joinGameNegativeTwoTest() throws ResponseException {
+        UserData userData = new UserData("a", "a", "a");
+        AuthData authData = serverFacade.register(userData);
+
+        serverFacade.createGame(new CreateGameRequest("newgame"), authData.authToken());
+
 
     }
 
     @Test
     @DisplayName("Join Game - negative")
     public void joinGameNegativeTest() throws ResponseException {
-
+        try{
+            serverFacade.joinGame(new JoinGameRequest("WHITE", "a", 2), "");
+            Assertions.fail("Shouldn't be able to access without valid auth token.");
+        } catch (ResponseException e) {
+            Assertions.assertEquals(401, e.getStatusCode(), "Status code 401 expected, because the auth never existed.");
+        }
     }
 
     @Test
     @DisplayName("Create Game - positive")
     public void createGamePositiveTest() throws ResponseException {
+        UserData userData = new UserData("a", "a", "a");
+        AuthData authData = serverFacade.register(userData);
 
+        serverFacade.createGame(new CreateGameRequest("newgame"), authData.authToken());
+        GameListResult games = serverFacade.listGames(authData.authToken());
+        Assertions.assertEquals(1, games.getGames().size());
     }
 
     @Test
     @DisplayName("Create Game - negative")
     public void createGameNegativeTest() throws ResponseException {
-
+        UserData userData = new UserData("a", "a", "a");
+        AuthData authData = serverFacade.register(userData);
+        try {
+            serverFacade.createGame(new CreateGameRequest(""), authData.authToken());
+            Assertions.fail("Shouldn't be able to create a blank game");
+        } catch (ResponseException e) {
+            Assertions.assertEquals(400, e.getStatusCode(), "Status code 400 expected, because you cant create a game with an empty name.");
+        }
     }
 
 
